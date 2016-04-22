05a7b4684afdf491e6a80fe969fbe7cc29df57ba
pitou35 <pierreaugustin.totaro03@gmail.com>
2016-04-22 16:02:01 +0200

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tpApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tpApp
+ */
+angular.module('tpApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tpApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tpApp
+ */
+angular.module('tpApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
